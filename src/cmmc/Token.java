44eaef2f1c32c3623f2e2a5a1528548fabbd2565
Liuhao <satansin3@gmail.com>
2013-10-25 17:46:09 +0800

    Test commit

---
+++ src/cmmc/Token.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cmmc;
+
+/**
+ * @author ºÆ
+ *
+ */
+public class Token {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
